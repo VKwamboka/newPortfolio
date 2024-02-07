0b38c9505163cbcf3f85c004f1160c907563573e
VKwamboka <verahnyagaka101@gmail.com>
2024-02-07 23:57:05 +0100

    added typing effect

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,11 @@
-import { Component, HostListener} from '@angular/core';
+import { Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
   isNavbarVisible = false;
 
@@ -15,4 +15,38 @@ export class HomeComponent {
   }
 
   isMenuCollapsed = true;
+
+  words: string[] = ['web developer', 'app developer'];
+  wordIndex: number = 0;
+  letterIndex: number = 0;
+  isDeleting: boolean = false;
+  text: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.typeText();
+  }
+
+  typeText(): void {
+    const currentWord = this.words[this.wordIndex];
+
+    if (this.isDeleting) {
+      this.text = currentWord.substring(0, --this.letterIndex);
+    } else {
+      this.text = currentWord.substring(0, ++this.letterIndex);
+    }
+
+    if (!this.isDeleting && this.text === currentWord) {
+      this.isDeleting = true;
+      setTimeout(() => this.typeText(), 1000);
+    } else if (this.isDeleting && this.text === '') {
+      this.isDeleting = false;
+      this.wordIndex = (this.wordIndex + 1) % this.words.length;
+      setTimeout(() => this.typeText(), 500);
+    } else {
+      const speed = this.isDeleting ? 50 : 100;
+      setTimeout(() => this.typeText(), speed);
+    }
+  }
 }
